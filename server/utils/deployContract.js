d2286839fb2fbbe066a6ba8e8ba5eaecb5c81681
Innovoeb <Innovoeb@gmail.com>
2024-06-22 09:34:00 -0700

    update deployContract.js from Ethersv5 => Ethersv6

--- server/utils/deployContract.js
+++ server/utils/deployContract.js
@@ -1,68 +1,92 @@
 const hre = require("hardhat")
 const { Wallet } = require("./wallets")
 const { DB } = require("../data/db")
+const { Artifacts } = require("./artifacts")
 
 
 module.exports.deployContract = async (contractName, network, params) => {
-    let signer, contractFactory, response
-    const artifacts = await hre.artifacts.readArtifact(contractName)
+    let contractFactory, response
 
     if (network === "local") {
-        const provider = new hre.ethers.providers.JsonRpcProvider()
-        signer = provider.getSigner()
-        contractFactory = new hre.ethers.ContractFactory(artifacts.abi, artifacts.bytecode, signer)
-
-        response = await deploymentResponse(contractFactory, params)
-        DB.postLocal("http://localhost:3001/local", responseOBJ(contractName, response, network))
+        contractFactory = new hre.ethers.ContractFactory(
+            await Artifacts.getABI(contractName), 
+            await Artifacts.getBytecode(contractName), 
+            Wallet.local
+        )
+        response = await deploy(contractFactory, params)
+        DB.post("http://localhost:3001/local-deployments", {
+            contractName: contractName,
+            contractAddress: response.contractAddress,
+            tx: response.hash,
+            network: network,
+            time: new Date().toISOString()
+        })
     } else if (network === "sepolia") {
-        contractFactory = new hre.ethers.ContractFactory(artifacts.abi, artifacts.bytecode, Wallet.sepolia)
-        
-        response = await deploymentResponse(contractFactory)
-        //DB.postTestnet("http://localhost:3001/testnet-deployments", responseOBJ(contractName, response, network))
+        contractFactory = new hre.ethers.ContractFactory(
+            await Artifacts.getABI(contractName), 
+            await Artifacts.getBytecode(contractName), 
+            Wallet.sepolia
+        )
+        response = await deploy(contractFactory, params)
+        DB.post("http://localhost:3001/testnet-deployments", {
+            contractName: contractName,
+            contractAddress: response.contractAddress,
+            tx: response.hash,
+            network: network,
+            time: new Date().toISOString()
+        })
     } else if (network === "polygon_amoy") {
-        contractFactory = new hre.ethers.ContractFactory(artifacts.abi, artifacts.bytecode, Wallet.polygon_amoy)
-        
-        response = await deploymentResponse(contractFactory)
-        //DB.postTestnet("http://localhost:3001/testnet-deployments", responseOBJ(contractName, response, network))
+        contractFactory = new hre.ethers.ContractFactory(
+            await Artifacts.getABI(contractName), 
+            await Artifacts.getBytecode(contractName), 
+            Wallet.polygon_amoy
+        )
+        response = await deploy(contractFactory, params)
+        DB.post("http://localhost:3001/testnet-deployments", {
+            contractName: contractName,
+            contractAddress: response.contractAddress,
+            tx: response.hash,
+            network: network,
+            time: new Date().toISOString()
+        })
     } else if (network === "arbitrum_sepolia") {
-        contractFactory = new hre.ethers.ContractFactory(artifacts.abi, artifacts.bytecode, Wallet.arbitrum_sepolia)
-        
-        response = await deploymentResponse(contractFactory)
-        //DB.postTestnet("http://localhost:3001/testnet-deployments", responseOBJ(contractName, response, network))
+        contractFactory = new hre.ethers.ContractFactory(
+            await Artifacts.getABI(contractName),
+            await Artifacts.getBytecode(contractName), 
+            Wallet.arbitrum_sepolia
+        )
+        response = await deploy(contractFactory)
+        DB.post("http://localhost:3001/testnet-deployments", {
+            contractName: contractName,
+            contractAddress: response.contractAddress,
+            tx: response.hash,
+            network: network,
+            time: new Date().toISOString()
+        })
     }
-    
     return response
 }
 
-const deploymentResponse = async (contractFactory, params) => {
+// handle contract constructor params during deployment
+const deploy = async (contractFactory, params) => {
     if (params.length == 0) {
         const contract = await contractFactory.deploy()
-        const response = await contract.deployTransaction.wait()
+        const response = await contract.deploymentTransaction().wait(1)
         return response
     } else if (params.length == 1) {
         const contract = await contractFactory.deploy(params[0])
-        const response = await contract.deployTransaction.wait()
+        const response = await contract.deploymentTransaction().wait(1)
         return response
     } else if (params.length == 2) {
         const contract = await contractFactory.deploy(params[0], params[1])
-        const response = await contract.deployTransaction.wait()
+        const response = await contract.deploymentTransaction().wait(1)
         return response
     } else if (params.length == 3) {
         const contract = await contractFactory.deploy(params[0], params[1], params[2])
-        const response = await contract.deployTransaction.wait()
+        const response = await contract.deploymentTransaction().wait(1)
         return response
     }
 }
 
-const responseOBJ = (contractName, response, network) => {
-    return {
-        type: "contract-deployment",
-        contractName: contractName,
-        contractAddress: `${response.contractAddress}`,
-        tx: `${response.transactionHash}`,
-        network: network,
-        time: new Date().toISOString()
-    }
-}
 
 
